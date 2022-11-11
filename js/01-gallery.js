b8c5b014b00760553b2b462bdf42c9554506a372
Markov Dmitriy Yurevich <d.markov@burdaua.vhbk>
2022-11-11 21:34:04 +0200

    сделал разметку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  container: document.querySelector(`.gallery`),
+};
+
+function createImgMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+    </div>`;
+    })
+    .join(``);
+}
+
+const imgMarkup = createImgMarkup(galleryItems);
+console.log(imgMarkup);
+
+refs.container.insertAdjacentHTML(`beforeend`, imgMarkup);
