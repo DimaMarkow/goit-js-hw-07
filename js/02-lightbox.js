3f1f6ac006eb06b1cdc35b9a42e81da16e67e833
Markov Dmitriy Yurevich <d.markov@burdaua.vhbk>
2022-11-12 15:04:25 +0200

    поставил SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  container: document.querySelector(`.gallery`),
+};
+
+const imgMarkup = createImgMarkup(galleryItems);
+
+refs.container.insertAdjacentHTML(`beforeend`, imgMarkup);
+
+refs.container.addEventListener(`click`, handleImgClick);
+
+function createImgMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join(``);
+}
+
+function handleImgClick(event) {
+  event.preventDefault();
+  //   if (!event.target.classList.contains(`gallery__image`)) {
+  //     return;
+  //   }
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: `alt`,
+    captionDelay: 250,
+  });
+}
